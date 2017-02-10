b562f27ee125d57d709d4fcd29e59eb12bf4b97c
joshuawilson <javajoshw@gmail.com>
2017-02-10 14:14:47 -0500

    update profile to not reference bad import, remove main dependencies from package

--- index.ts
+++ index.ts
@@ -2,4 +2,6 @@ export { AuthenticationService } from './src/app/auth/authentication.service';
 export { Broadcaster } from './src/app/shared/broadcaster.service';
 export { Logger }      from './src/app/shared/logger.service';
 export { UserService } from './src/app/user/user.service';
+export { Entity } from './src/app/user/entity';
+export { Profile } from './src/app/user/profile';
 export { User } from './src/app/user/user';
--- src/app/user/profile.ts
+++ src/app/user/profile.ts
@@ -1,4 +1,4 @@
-import { Email } from './email';
+// import { Email } from './email';
 
 export class Profile {
     fullName: string;
