e6ee47cd17adba344966a053ab4b8f38db44626d
Pete Muir <pmuir@bleepbleep.org.uk>
2017-03-05 09:11:50 -0500

    docs(broadcaster): Document broadcaster

--- src/app/shared/broadcaster.service.ts
+++ src/app/shared/broadcaster.service.ts
@@ -9,6 +9,35 @@ interface BroadcastEvent {
   data?: any;
 }
 
+/**
+ * Provides detyped notifications of changes in application state.
+ *
+ * It is recommended that all components and services broadcast events
+ * that may be of interest to others. There is no overhead to broadcasting
+ * an event that no one is subscribed to.
+ *
+ * Any events broadcast should be documented as part of the modules API.
+ *
+ * Example
+ * -------
+ *
+ * In this example we broadcast an event of type cheese and have the mouse
+ * subscribe to it.
+ *
+ *
+ *     constructor(
+ *       private broadcaster: Broadcaster
+ *     )
+ *
+ *     ngOnInit() {
+ *       broadcaster.on<Cheese>('cheese').subscribe(val => this.mouse.eatCheese(val));
+ *     }
+ *
+ *     newCheese() {
+ *       broadcaster.broadcast('cheese', { type: 'cheddar' } as Cheese);
+ *     }
+ *
+ */
 @Injectable()
 export class Broadcaster {
   private _eventBus: Subject<BroadcastEvent>;
@@ -17,10 +46,23 @@ export class Broadcaster {
     this._eventBus = new Subject<BroadcastEvent>();
   }
 
+  /**
+   * Broadcast an event.
+   *
+   * @param key the key to broadcast the event for. Normally we use a string.
+   * @param data the payload to send.
+   */
   broadcast(key: any, data?: any) {
     this._eventBus.next({key, data});
   }
 
+  /**
+   * Observe an event.
+   *
+   * @param key the key to observe the event for.
+   * @returns an Observable to which you can subscribe or use any RxJS operator.
+   *
+   */
   on<T>(key: any): Observable<T> {
     return this._eventBus.asObservable()
       .filter(event => event.key === key)
