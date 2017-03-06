563d334da556f0d307cd00d2c007823afcc5d239
Dana Gutride <dgutride@redhat.com>
2017-03-06 14:48:31 -0500

    fix(redesignusers): redesigns user service to use observables instead of promises
    fixes: issue 5

--- src/app/user/user.service.ts
+++ src/app/user/user.service.ts
@@ -2,14 +2,21 @@ import { Injectable, Inject } from '@angular/core';
 import { Headers, Http } from '@angular/http';
 
 import 'rxjs/add/operator/toPromise';
+import 'rxjs/add/operator/catch';
 
 import { Broadcaster } from '../shared/broadcaster.service';
 import { AuthenticationService } from '../auth/authentication.service';
 import { Logger } from '../shared/logger.service';
 import { AUTH_API_URL } from '../shared/auth-api';
 import { User } from './user';
+import { Observable } from 'rxjs';
 
-//import { ProfileService } from './../profile/profile.service';
+/**
+ *  Provides user and user list methods to retrieve current or user list details
+ *
+ *  The UserService should be injected at the root of the application to ensure it is a singleton
+ *  getUser and getAllUsers return observables that can be subscribed to for information
+ */
 
 @Injectable()
 export class UserService {
@@ -44,19 +51,21 @@ export class UserService {
     return this.allUserData;
   }
 
-  getUser(): Promise<User> {
+  /**
+   * Get currently logged in user
+   *
+   * @returns Observable<User>
+   */
+  getUser(): Observable<User> {
     // Check if we have the user data if not then check if the user is logged in.
     // We need the auth key to get the user data. So either we already the data or we don't have the keys
     //   in either case don't try to get the data.
     if (Object.keys(this.userData).length || !this.auth.isLoggedIn()) {
-      return new Promise((resolve, reject) => {
-        resolve(this.userData);
-      });
+      return Observable.create(this.userData);
     } else {
       return this.http
         .get(this.userUrl, {headers: this.headers})
-        .toPromise()
-        .then(response => {
+        .map(response => {
           let userData = response.json().data as User;
           // The reference of this.userData is
           // being used in Header
@@ -70,27 +79,26 @@ export class UserService {
           };
           this.userData.id = userData.id;
           // this.profile.initDefaults(this.userData);
-          this.broadcaster.broadcast('currentUserInit', this.userData);
           return this.userData;
-        })
-        .catch(() => {});
+        });
     }
   }
 
-  getAllUsers(): Promise<User[]> {
+  /**
+   * Get all users
+   *
+   * @returns Observable<User[]>
+   */
+  getAllUsers(): Observable<User[]> {
     if (this.allUserData.length) {
-      return new Promise((resolve, reject) => {
-        resolve(this.allUserData);
-      });
+      return Observable.create(this.allUserData);
     } else {
       return this.http
         .get(this.usersUrl, {headers: this.headers})
-        .toPromise()
-        .then(response => {
+        .map(response => {
           this.allUserData = response.json().data as User[];
           return this.allUserData;
-        })
-        .catch(() => {});
+        });
     }
   }
 
