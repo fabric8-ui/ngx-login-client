0f26c52af2c1afd5dd65648f572d896516bb14c6
Pete Muir <pmuir@bleepbleep.org.uk>
2017-03-16 20:12:46 -0400

    fix(profile): make profile match data model

--- src/app/user/profile.ts
+++ src/app/user/profile.ts
@@ -1,18 +1,10 @@
 // import { Email } from './email';
 
 export class Profile {
-    fullName: string;
-    imageURL: string;
-    username: string;
-    bio?: string;
-    url?: string;
-    provider?: string;
-    email?: string;
-    emails?: string[];
-    primaryEmail?: string;
-    notificationEmail?: string;
-    publicEmail?: string;
-    primaryEmailPrivate?: boolean;
-    emailPreference?: string;
-    notificationMethods?: string[];
+  fullName: string;
+  imageURL: string;
+  username: string;
+  bio?: string;
+  url?: string;
+  email?: string;
 }
