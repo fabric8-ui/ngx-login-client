f9f6e276114d8835fe74e3a580fd93b8d570f9df
Dana Gutride <dgutride@redhat.com>
2017-03-17 16:38:09 -0400

    fix(ostoken): add openshift token

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 export { LoginModule } from './login.module';
 
 export { AUTH_API_URL } from './src/app/shared/auth-api';
+export { SSO_API_URL } from './src/app/shared/sso-api';
 export { AlmUserName } from './src/app/user/alm-user-name.pipe';
 export { AuthenticationService } from './src/app/auth/authentication.service';
 export { Broadcaster } from './src/app/shared/broadcaster.service';
---
+++ src/app/shared/sso-api.ts
@@ -0,0 +1,3 @@
+import { OpaqueToken } from '@angular/core';
+
+export let SSO_API_URL = new OpaqueToken('sso.api.url');
