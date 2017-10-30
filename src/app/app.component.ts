7e79c30221bdf852483dec4d7eea32cfd95e6d0f
joshuawilson <javajoshw@gmail.com>
2017-10-30 15:32:47 -0400

    fix(build): remove old reference to .scss

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,6 @@ import { Component, ViewEncapsulation } from '@angular/core';
   encapsulation: ViewEncapsulation.None,
   host: {'class': 'app app-component flex-container in-column-direction flex-grow-1'},
   selector: 'demo-app',
-  styleUrls: ['./app.component.scss'],
   templateUrl: './app.component.html'
 })
 export class AppComponent {
