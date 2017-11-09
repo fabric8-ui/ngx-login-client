5e4a6a3b305030b162658b151358a2b6c026eeab
shoubhik bose <sbose78@gmail.com>
2017-11-10 00:45:51 +0530

    fix(http): allow 401 if www-authenticate header present

--- src/app/shared/http.service.ts
+++ src/app/shared/http.service.ts
@@ -46,7 +46,7 @@ export class HttpService extends Http {
 
   private catchRequestError () {
     return (res: Response) => {
-      if (res.status === 401 || res.status === 403) {
+      if (res.status === 403 || ( res.status === 401 && !res.headers.has("www-authenticate"))) {
         this.broadcaster.broadcast('authenticationError', res);
       } else if (res.status === 500) {
         this.broadcaster.broadcast('communicationError', res);
