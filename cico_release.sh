52d610ef365b70151bd1667f7913db69bba52197
joshuawilson <javajoshw@gmail.com>
2018-08-23 14:38:30 -0400

    fix(build): set log level to debug for build script

--- cico_release.sh
+++ cico_release.sh
@@ -16,7 +16,7 @@ function release() {
     npm whoami
 
     # Enable verbose output
-    npm config set loglevel verbose
+    npm config set loglevel debug
 
     # Build and Release Planner (It will update the tag on github and push fabric8-planner to npmjs.org)
     npm run semantic-release
