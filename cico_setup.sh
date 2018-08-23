bc84660263087614a9258c9adbda9fa8553262cc
Roman Iuvshyn <riuvshyn@redhat.com>
2018-08-23 15:59:56 +0300

    Update cico_setup.sh

--- cico_setup.sh
+++ cico_setup.sh
@@ -19,6 +19,7 @@ prep() {
   yum -y install docker make gcc-c++ bzip2 fontconfig rh-git29
   yum -y install centos-release-scl
   yum -y install sclo-git212.x86_64
+  scl enable sclo-git212 cico_release.sh
   curl -sL https://rpm.nodesource.com/setup_8.x | sudo -E bash -
   yum -y install nodejs
 
@@ -69,7 +70,6 @@ build_project() {
   fi
 }
 
-scl enable sclo-git212 cico_release.sh
 . cico_release.sh
 
 load_jenkins_vars
